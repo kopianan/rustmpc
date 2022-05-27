125a0c2ce455a0ffecc8194d8e20eaddde658f4f
Rwinwij <erwin.wijaya@coinbit.id>
2022-05-27 10:46:47 +0800

    replace code generator vector<u8> implementation

--- gg18-multi-party-ecdsa-ffi/src/lib.rs
+++ gg18-multi-party-ecdsa-ffi/src/lib.rs
@@ -1,17 +1,24 @@
 #![allow(clippy::missing_safety_doc, clippy::not_unsafe_ptr_arg_deref)]
 
 use allo_isolate::Isolate;
-use flutter_rust_bridge::*;
 use async_ffi::{FfiFuture, FutureExt};
+use std::os::raw::{c_char, c_uchar};
+use core::slice;
 
 #[no_mangle]
 pub extern "C" fn wire_keygen(
     port_: i64,
-    secrets_byte_vec: *mut wire_uint_8_list,
-    group_byte_vec: *mut wire_uint_8_list,
+    secrets_byte_vec: *const c_uchar,
+    secrets_byte_len: usize,
+    group_byte_vec: *const c_uchar,
+    group_byte_len: usize,
 ) {
-    let api_secrets_byte_vec = secrets_byte_vec.wire2api();
-    let api_group_byte_vec = group_byte_vec.wire2api();
+    let secrets_byte_vec = unsafe {slice::from_raw_parts(secrets_byte_vec, secrets_byte_len)};
+    let mut api_secrets_byte_vec: Vec<u8> = Vec::from(secrets_byte_vec);
+    
+    let group_byte_vec = unsafe {slice::from_raw_parts(group_byte_vec, group_byte_len)};
+    let mut api_group_byte_vec: Vec<u8> = Vec::from(group_byte_vec);
+
     async move {
         let result = gg18_multi_party_ecdsa::keygen(api_secrets_byte_vec, api_group_byte_vec).await;
         // make a ref to an isolate using it's port
@@ -21,62 +28,3 @@ pub extern "C" fn wire_keygen(
         isolate.post(result);
     }.into_local_ffi();
 }
-
-// Section: wire structs
-
-#[repr(C)]
-#[derive(Clone)]
-pub struct wire_uint_8_list {
-    ptr: *mut u8,
-    len: i32,
-}
-
-// Section: wrapper structs
-
-// Section: static checks
-
-// Section: allocate functions
-
-#[no_mangle]
-pub extern "C" fn new_uint_8_list(len: i32) -> *mut wire_uint_8_list {
-    let ans = wire_uint_8_list {
-        ptr: support::new_leak_vec_ptr(Default::default(), len),
-        len,
-    };
-    support::new_leak_box_ptr(ans)
-}
-
-// Section: impl Wire2Api
-
-pub trait Wire2Api<T> {
-    fn wire2api(self) -> T;
-}
-
-impl<T, S> Wire2Api<Option<T>> for *mut S
-where
-    *mut S: Wire2Api<T>,
-{
-    fn wire2api(self) -> Option<T> {
-        if self.is_null() {
-            None
-        } else {
-            Some(self.wire2api())
-        }
-    }
-}
-
-impl Wire2Api<u8> for u8 {
-    fn wire2api(self) -> u8 {
-        self
-    }
-}
-
-impl Wire2Api<Vec<u8>> for *mut wire_uint_8_list {
-    fn wire2api(self) -> Vec<u8> {
-        unsafe {
-            let wrap = support::box_from_leak_ptr(self);
-            support::vec_from_leak_ptr(wrap.ptr, wrap.len)
-        }
-    }
-}
-
