d83c0bc0d9523a1d8e4c9789e919497d978cfd1d
kopianan <ananalfredcarlos@gmail.com>
2022-06-04 18:22:03 +0700

    Revert "ffi binding"
    This reverts commit f2ea461aa461232250b3f1384ff297ee22e43763.

--- gg20-mpc-ffi/src/lib.rs
+++ gg20-mpc-ffi/src/lib.rs
@@ -2,14 +2,14 @@
 
 use allo_isolate::Isolate;
 use async_ffi::{FfiFuture, FutureExt};
-use core::slice;
 use std::os::raw::{c_char, c_uchar};
+use core::slice;
 
 use gg20_mpc::*;
 
-use anyhow::{anyhow, bail, ensure, Context, Result};
-use std::{fs, vec::Vec};
+use anyhow::{ anyhow, bail, ensure, Context, Result};
 use structopt::StructOpt;
+use std::{fs, vec::Vec};
 
 use lazy_static::lazy_static;
 use std::{ffi::CStr, io, os::raw};
@@ -36,33 +36,29 @@ macro_rules! runtime {
 }
 
 //MPC CONSTANTS
-const THRESHOLD: u16 = 1;
-const PARTIES: u16 = 3;
+const THRESHOLD:u16 = 1;
+const PARTIES:u16 = 3;
 
 #[no_mangle]
-pub extern "C" fn http_local_run(port_: i64) {
+pub extern "C" fn http_local_run() {
     let rt = runtime!();
     let http_task = async move {
-        let isolate = Isolate::new(port_);
-        isolate.post("start");
         let result = gg20_mpc::http_local_run().await;
-        isolate.post("completed");
-    }
-    .into_ffi();
+    }.into_ffi();
     rt.spawn(http_task);
 }
 
 #[no_mangle]
-pub extern "C" fn wire_keygen(port_: i64, index: u16) {
+pub extern "C" fn wire_keygen(
+    port_: i64,
+    index: u16,
+) {
     let rt = runtime!();
     let keygen_task = async move {
         let isolate = Isolate::new(port_);
-        isolate.post("test");
         let result = gg20_mpc::keygen_run(index).await;
-        isolate.post("test");
-        // isolate.post(result);
-    }
-    .into_ffi();
+        isolate.post(result);
+    }.into_ffi();
 
     rt.spawn(keygen_task);
 }
